72b23a69540a05a2c0e5114509def469adcb4d00
yb9255 <yoobin9255@gmail.com>
2026-01-08 17:03:24 +0900

    Cart 프로덕트 생성

---
+++ cart/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8082,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'cart',
+      filename: 'remoteEntry.js',
+      exposes: { './CartShow': './src/index' },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
